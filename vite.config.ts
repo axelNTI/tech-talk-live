e8f44af65e9ffce327af04aff8ebf375064dfecc
axelNTI <axel.thornberg@elev.ga.ntig.se>
2025-05-27 21:50:17 +0200

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        port: 3000,
-        strictPort: true,
-        host: 'localhost',
-    },
-})
+	server: {
+		port: 3000,
+		strictPort: true,
+		host: "localhost",
+	},
+});
