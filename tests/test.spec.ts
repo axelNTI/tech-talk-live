6b5146a8f42be6b68c3ed401c2a8e3a53419b93c
Axel Thornberg <axel.thornberg@elev.ga.ntig.se>
2025-05-28 08:40:54 +0200

    Final change

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from "@playwright/test";
 test("Hello, World", async ({ page }) => {
 	await page.goto("/");
 
-	await expect(page.getByText("Hello, World")).toBeVisible();
+	await expect(page.getByText("Hello, TE4")).toBeVisible();
 });
